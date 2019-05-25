4ced31ac4e3162c121fbc2d02c1f164797befbc0
xuancuong98bn <xuancuong98bn@gmail.com>
2019-05-26 00:56:56 +0700

    Add Lab 1 part 3

---
+++ project/Lab1/Lab1_FormAdvanced/Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace Lab1_FormAdvanced
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.radVi = new System.Windows.Forms.RadioButton();
+            this.radEn = new System.Windows.Forms.RadioButton();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.lblForgot = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblRequiredUser = new System.Windows.Forms.Label();
+            this.lblRequiredPass = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lblForgot);
+            this.panel1.Controls.Add(this.lblTitle);
+            this.panel1.Controls.Add(this.panel4);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(384, 238);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.radEn);
+            this.panel2.Controls.Add(this.radVi);
+            this.panel2.Location = new System.Drawing.Point(263, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(102, 27);
+            this.panel2.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.lblRequiredPass);
+            this.panel3.Controls.Add(this.lblRequiredUser);
+            this.panel3.Controls.Add(this.txtPassword);
+            this.panel3.Controls.Add(this.txtUsername);
+            this.panel3.Controls.Add(this.lblPassword);
+            this.panel3.Controls.Add(this.lblUsername);
+            this.panel3.Location = new System.Drawing.Point(20, 80);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(345, 65);
+            this.panel3.TabIndex = 1;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.btnExit);
+            this.panel4.Controls.Add(this.btnNew);
+            this.panel4.Controls.Add(this.btnLogin);
+            this.panel4.Location = new System.Drawing.Point(20, 151);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(345, 33);
+            this.panel4.TabIndex = 2;
+            // 
+            // radVi
+            // 
+            this.radVi.AutoSize = true;
+            this.radVi.Checked = true;
+            this.radVi.Location = new System.Drawing.Point(3, 4);
+            this.radVi.Name = "radVi";
+            this.radVi.Size = new System.Drawing.Size(43, 17);
+            this.radVi.TabIndex = 0;
+            this.radVi.TabStop = true;
+            this.radVi.Text = "Việt";
+            this.radVi.UseVisualStyleBackColor = true;
+            // 
+            // radEn
+            // 
+            this.radEn.AutoSize = true;
+            this.radEn.Location = new System.Drawing.Point(52, 4);
+            this.radEn.Name = "radEn";
+            this.radEn.Size = new System.Drawing.Size(44, 17);
+            this.radEn.TabIndex = 1;
+            this.radEn.Text = "Anh";
+            this.radEn.UseVisualStyleBackColor = true;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.Red;
+            this.lblTitle.Location = new System.Drawing.Point(129, 43);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(138, 25);
+            this.lblTitle.TabIndex = 3;
+            this.lblTitle.Text = "ĐĂNG NHẬP";
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsername.Location = new System.Drawing.Point(4, 11);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(123, 17);
+            this.lblUsername.TabIndex = 0;
+            this.lblUsername.Text = "Tên đăng nhập:";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPassword.Location = new System.Drawing.Point(4, 39);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(79, 17);
+            this.lblPassword.TabIndex = 1;
+            this.lblPassword.Text = "Mật khẩu:";
+            // 
+            // lblForgot
+            // 
+            this.lblForgot.AutoSize = true;
+            this.lblForgot.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Italic | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblForgot.ForeColor = System.Drawing.Color.Blue;
+            this.lblForgot.Location = new System.Drawing.Point(149, 201);
+            this.lblForgot.Name = "lblForgot";
+            this.lblForgot.Size = new System.Drawing.Size(89, 13);
+            this.lblForgot.TabIndex = 4;
+            this.lblForgot.Text = "Quên mật khẩu ?";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(125, 10);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(181, 20);
+            this.txtUsername.TabIndex = 2;
+            this.txtUsername.TextChanged += new System.EventHandler(this.txtUsername_TextChanged);
+            this.txtUsername.Enter += new System.EventHandler(this.txtUsername_Enter);
+            this.txtUsername.Leave += new System.EventHandler(this.txtUsername_Leave);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(125, 38);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(181, 20);
+            this.txtPassword.TabIndex = 3;
+            this.txtPassword.TextChanged += new System.EventHandler(this.txtPassword_TextChanged);
+            this.txtPassword.Enter += new System.EventHandler(this.txtPassword_Enter);
+            this.txtPassword.Leave += new System.EventHandler(this.txtPassword_Leave);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.ForeColor = System.Drawing.Color.Purple;
+            this.btnLogin.Location = new System.Drawing.Point(7, 4);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(93, 23);
+            this.btnLogin.TabIndex = 0;
+            this.btnLogin.Text = "ĐĂNG NHẬP";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // btnNew
+            // 
+            this.btnNew.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNew.ForeColor = System.Drawing.Color.Purple;
+            this.btnNew.Location = new System.Drawing.Point(125, 4);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(93, 23);
+            this.btnNew.TabIndex = 1;
+            this.btnNew.Text = "LÀM MỚI";
+            this.btnNew.UseVisualStyleBackColor = true;
+            this.btnNew.Click += new System.EventHandler(this.btnNew_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.ForeColor = System.Drawing.Color.Purple;
+            this.btnExit.Location = new System.Drawing.Point(246, 4);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(93, 23);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "THOÁT";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblRequiredUser
+            // 
+            this.lblRequiredUser.AutoSize = true;
+            this.lblRequiredUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRequiredUser.ForeColor = System.Drawing.Color.Red;
+            this.lblRequiredUser.Location = new System.Drawing.Point(312, 11);
+            this.lblRequiredUser.Name = "lblRequiredUser";
+            this.lblRequiredUser.Size = new System.Drawing.Size(15, 20);
+            this.lblRequiredUser.TabIndex = 4;
+            this.lblRequiredUser.Text = "*";
+            // 
+            // lblRequiredPass
+            // 
+            this.lblRequiredPass.AutoSize = true;
+            this.lblRequiredPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRequiredPass.ForeColor = System.Drawing.Color.Red;
+            this.lblRequiredPass.Location = new System.Drawing.Point(312, 39);
+            this.lblRequiredPass.Name = "lblRequiredPass";
+            this.lblRequiredPass.Size = new System.Drawing.Size(15, 20);
+            this.lblRequiredPass.TabIndex = 5;
+            this.lblRequiredPass.Text = "*";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(233)))), ((int)(((byte)(216)))));
+            this.ClientSize = new System.Drawing.Size(405, 261);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "ĐĂNG NHẬP HỆ THỐNG";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.RadioButton radVi;
+        private System.Windows.Forms.Label lblForgot;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.RadioButton radEn;
+        private System.Windows.Forms.Label lblRequiredPass;
+        private System.Windows.Forms.Label lblRequiredUser;
+    }
+}
+
---
+++ project/Lab1/Lab1_FormAdvanced/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Lab1_FormAdvanced
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtUsername_Enter(object sender, EventArgs e)
+        {
+            txtUsername.BackColor = Color.Pink;
+        }
+
+        private void txtUsername_Leave(object sender, EventArgs e)
+        {
+            txtUsername.BackColor = Color.White;
+        }
+
+        private void txtPassword_Enter(object sender, EventArgs e)
+        {
+            txtPassword.BackColor = Color.Pink;
+        }
+
+        private void txtPassword_Leave(object sender, EventArgs e)
+        {
+            txtPassword.BackColor = Color.White;
+        }
+
+        private void txtUsername_TextChanged(object sender, EventArgs e)
+        {
+            if (txtUsername.Text.Trim() == "")
+            {
+                lblRequiredUser.ForeColor = Color.Red;
+            }
+            else
+            {
+                lblRequiredUser.ForeColor = Color.Blue;
+            }
+        }
+
+        private void txtPassword_TextChanged(object sender, EventArgs e)
+        {
+            if (txtPassword.Text.Trim() == "")
+            {
+                lblRequiredPass.ForeColor = Color.Red;
+            }
+            else
+            {
+                lblRequiredPass.ForeColor = Color.Blue;
+            }
+        }
+
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            txtUsername.Text = "";
+            txtPassword.Text = "";
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Bạn có muốn thoát?", "Thông báo", MessageBoxButtons.OKCancel);
+            if (result == DialogResult.OK)
+            {
+                Close();
+            }
+        }
+
+        private bool CheckInput()
+        {
+            if (txtUsername.Text.Trim() == "")
+            {
+                MessageBox.Show("Tên đăng nhập không được để trống", "Cảnh báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+            if (txtPassword.Text.Trim() == "")
+            {
+                MessageBox.Show("Mật khẩu không được để trống", "Cảnh báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+            return true;
+        }
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (CheckInput())
+            {
+                if ("Admin".Equals(txtUsername.Text) && "123456".Equals(txtPassword.Text))
+                {
+
+                }
+            }
+        }
+    }
+}
